01a2cd785705e1ea409c726c3a47ece5ffec05fb
KatL12 <163588064+KatL12@users.noreply.github.com>
2024-03-24 20:09:27 +0100

    zadanie domowe 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var isWoman = true;
+var age = 28;
+
+if (isWoman == true && age < 30)
+
+    Console.WriteLine("Kobieta ponizej 30 lat");
+
+else if (name == "Ewa" && age == 30)
+
+    Console.WriteLine("Ewa lat 30");
+
+else if (isWoman != true && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");   
+}
+
+
